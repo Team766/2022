f8b591e3ca477d37bb48046526d6daa4c7260f93
Prem Netsuwan <31556469+grnnja@users.noreply.github.com>
2022-02-20 16:09:28 -0800

    Added DigitalInputReaders

--- src/main/java/com/team766/frc2022/mechanisms/Elevator.java
+++ src/main/java/com/team766/frc2022/mechanisms/Elevator.java
@@ -1,9 +1,9 @@
 package com.team766.frc2022.mechanisms;
-
 import com.team766.config.ConfigFileReader;
 import com.team766.framework.Mechanism;
 import com.team766.hal.RobotProvider;
 import com.team766.hal.CANSpeedController;
+import com.team766.hal.DigitalInputReader;
 import com.team766.hal.SolenoidController;
 import com.team766.library.ValueProvider;
 import com.team766.logging.Category;
@@ -12,6 +12,9 @@ public class Elevator extends Mechanism {
     // Declaration of Mechanisms
 	private CANSpeedController m_elevator;
     private SolenoidController m_arms;
+    private DigitalInputReader m_bottom;
+    private DigitalInputReader m_top;
+
     private ValueProvider<Integer> elevatorTop;
     private ValueProvider<Integer> elevatorBottom;
     private ValueProvider<Integer> elevatorLeniency;
@@ -22,6 +25,9 @@ public class Elevator extends Mechanism {
         // Initializations
 		m_elevator = RobotProvider.instance.getTalonCANMotor("climber.elevator");
         m_arms = RobotProvider.instance.getSolenoid("climber.arms");
+        m_bottom = RobotProvider.instance.getDigitalInput("climber.bottomDigitalInput");
+        m_top = RobotProvider.instance.getDigitalInput("climber.bottomDigitalInput");
+
         elevatorTop = ConfigFileReader.getInstance().getInt("climber.elevatorTop"); //670
         elevatorBottom = ConfigFileReader.getInstance().getInt("climber.elevatorBottom"); //0
         elevatorLeniency = ConfigFileReader.getInstance().getInt("climber.elevatorLeniency"); //5
@@ -34,11 +40,11 @@ public class Elevator extends Mechanism {
         loggerCategory = Category.DRIVE;
         power *= elevatorPower.get();
         checkContextOwnership();
-        if (power < 0 && getElevatorPosition() - getElevatorBottom() >= getElevatorLeniency()) {
+        if (power < 0 && getElevatorPosition() - getElevatorBottom() >= getElevatorLeniency() && !m_bottom.get()) {
             m_elevator.set(power);
-        } else if (power > 0 && getElevatorTop() - getElevatorPosition() >= getElevatorLeniency()) {
+        } else if (power > 0 && getElevatorTop() - getElevatorPosition() >= getElevatorLeniency() && !m_top.get()) {
             m_elevator.set(power);
-        } else if (power != 0) {
+        } else {
             m_elevator.set(0);
         }
     }
