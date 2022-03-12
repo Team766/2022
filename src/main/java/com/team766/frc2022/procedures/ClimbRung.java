6d5adc9bb2901bb057299bd0a08e116ceb5a99b3
Prem Netsuwan <bcnofnenamg@gmail.com>
2022-03-12 15:05:56 -0800

    V2 climber

--- src/main/java/com/team766/frc2022/procedures/ClimbRung.java
+++ src/main/java/com/team766/frc2022/procedures/ClimbRung.java
@@ -16,12 +16,16 @@ public class ClimbRung extends Procedure {
 		context.waitForSeconds(0.5);
 		// Pull the Robot Up
 		log("Pull robot up");
-		Robot.elevator.setElevatorPower(-1.0);
-		context.waitForSeconds(5);
+		Robot.elevator.setElevatorPower(-1);
+		while(Math.abs(Robot.elevator.getElevatorPosition()) > 1300 ){
+			if(Robot.elevator.getLimitSwitchBottom()) break;
+			context.waitForSeconds(0.2);
+		};
+		Robot.elevator.setElevatorPower(0);
 		//Arms Grab Bar
 		log("Arms grab bar");
 		Robot.elevator.setArmsPower(1.0);
-		context.waitForSeconds(1.5);
+		context.waitForSeconds(0.5);
 		
 		for (int i = 0; i < 2; i++) {
 
@@ -44,7 +48,7 @@ public class ClimbRung extends Procedure {
 			Robot.elevator.setElevatorPower(0);
 			log("Extend arms");
 			Robot.elevator.setArmsPower(-1.0);
-			context.waitForSeconds(1.5);
+			context.waitForSeconds(1);
 			log("Retract elevator");
 			Robot.elevator.setElevatorPower(-1);
 			while(Math.abs(Robot.elevator.getElevatorPosition()) > 1300 ){
@@ -54,7 +58,7 @@ public class ClimbRung extends Procedure {
 			Robot.elevator.setElevatorPower(0);
 			log("Retract arms");
 			Robot.elevator.setArmsPower(1.0);
-			context.waitForSeconds(3.0);
+			context.waitForSeconds(1.0);
 		}
 
 
