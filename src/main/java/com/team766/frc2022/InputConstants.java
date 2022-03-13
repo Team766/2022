fd28128bf0d8c7f1a28174465f99b609b4484631
dgcodez <dg@voiceme.net>
2022-03-13 16:13:14 -0700

    Create InputConstants.java
    extracting OI constants into new class

---
+++ src/main/java/com/team766/frc2022/InputConstants.java
@@ -0,0 +1,36 @@
+/**
+ * Constants used for the Operator Interface, eg for joyticks, buttons, dials, etc.
+ * 
+ * TODO: consider moving this into a config file.
+ */
+public final class InputConstants {
+
+	// "joysticks"
+	public static final int LEFT_JOYSTICK = 0;
+	public static final int RIGHT_JOYSTICK = 1;
+	public static final int CONTROL_PANEL = 2;
+
+	// navigation
+	public static final int AXIS_LEFT_RIGHT = 0;
+	public static final int AXIS_FORWARD_BACKWARD = 1;
+
+	
+	// other ops
+	public static final int AXIS_SHOOTER_DIAL = 3;
+
+	public static final int CONTROL_PANEL_SHOOTER_SWITCH = 1;
+	public static final int CONTROL_PANEL_INTAKE_BUTTON = 2;
+	
+	public static final int CONTROL_PANEL_INTAKE_BUTTON = 2;
+	public static final int CONTROL_PANEL_BELTS_BUTTON = 3;
+
+	public static final int CONTROL_PANEL_SPITBALL_BUTTON = 14;
+
+	// TODO: update these
+	public static final int CONTROL_PANEL_AUTOCLIMB_BUTTON = 8;
+	public static final int CONTROL_PANEL_ARMS_SWITCH = 11;
+	public static final int CONTROL_PANEL_ELEVATOR_UP_BUTTON = 15;
+	public static final int CONTROL_PANEL_ELEVATOR_DOWN_BUTTON = 16;
+	public static final int CONTROL_PANEL_ELEVATOR_TOP_BUTTON = 17;
+	public static final int CONTROL_PANEL_ELEVATOR_BOTTOM_BUTTON = 18;
+}
