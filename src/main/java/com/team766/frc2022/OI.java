5dae2f6cec8d20ed6a7d7a776adbd1eb7a17f2f7
Prem Netsuwan <bcnofnenamg@gmail.com>
2022-03-24 14:25:11 -0700

    added OI button to climb all 3 rungs

--- src/main/java/com/team766/frc2022/OI.java
+++ src/main/java/com/team766/frc2022/OI.java
@@ -101,6 +101,10 @@ public class OI extends Procedure {
 				context.releaseOwnership(Robot.elevator);
 			}
 
+			if(m_ControlPanel.getButtonPressed(InputConstants.CONTROL_PANEL_AUTOCLIMB_BUTTON)){
+				context.startAsync(new Climb());
+			}
+
 			if (m_leftJoystick.getButtonPressed(InputConstants.JOYSTICK_ELEVATOR_RESET_BUTTON)){
 				//context.takeOwnership(Robot.shooter);
 				context.startAsync(new ResetElevator());
@@ -108,6 +112,10 @@ public class OI extends Procedure {
 
 			if(m_leftJoystick.getButtonPressed(InputConstants.JOYSTICK_CLIMB_RUNG_BUTTON)){
 				context.startAsync(new ClimbRung());
+			}
+
+			if(m_leftJoystick.getButtonPressed(InputConstants.JOYSTICK_CLIMB_FIRST_RUNG_BUTTON)){
+				context.startAsync(new ClimbFirstRung());
 			}
 			// 
 			//log(""+Robot.elevator.getElevatorPosition());
