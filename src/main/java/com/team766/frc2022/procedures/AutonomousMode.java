fbe555b731d9050baa97503c58b61563da385540
nate19428 <nicholaslchang@gmail.com>
2022-03-25 11:20:21 -0700

    Autonomous changes

--- src/main/java/com/team766/frc2022/procedures/AutonomousMode.java
+++ src/main/java/com/team766/frc2022/procedures/AutonomousMode.java
@@ -18,16 +18,8 @@ public class AutonomousMode extends Procedure{
 		context.waitForSeconds(0.25);
 		Robot.drive.setArcadeDrivePower(0,0);
 		context.waitForSeconds(0.5);
-		log(""+3);
-		if (3 != 0){
-			double power = ShooterVelociltyUtil.computeVelocityForDistance(3);
+		double power = ShooterVelociltyUtil.computeVelocityForDistance(3);
 			Robot.shooter.setVelocity(power);
-			if(power == 0.0) {
-				log("out of range");
-			} else {
-				log("set velocity to " + power);
-			}
-		}
 		context.waitForSeconds(5);
 		Robot.belts.startBelts();
 		context.waitForSeconds(2);
