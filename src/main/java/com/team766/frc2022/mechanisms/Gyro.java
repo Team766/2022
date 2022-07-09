f49bc178f7ea54530530c810462ee775fa620d8d
frenchyalex <apocesar11@gmail.com>
2022-07-09 14:21:22 -0700

    Pigeon
    Update for pigeon

--- src/main/java/com/team766/frc2022/mechanisms/Gyro.java
+++ src/main/java/com/team766/frc2022/mechanisms/Gyro.java
@@ -10,12 +10,49 @@ import com.team766.hal.SpeedController;
 import com.team766.library.RateLimiter;
 import com.team766.hal.CANSpeedController;
 import com.team766.logging.Category;
-import edu.wpi.first.wpilibj.I2C.Port;
-import com.team766.hal.GyroReader;
-import com.kauailabs.navx.frc.*;
+//import edu.wpi.first.wpilibj.I2C.Port;
+//import com.team766.hal.GyroReader;
+//import com.kauailabs.navx.frc.*;
+import com.ctre.phoenix.sensors.Pigeon2;
 
 public class Gyro extends Mechanism {
-	private AHRS m_gyro;
+	Pigeon2 g_gyro = new Pigeon2(0);
+	double[] gyroArray = new double[3];
+	private RateLimiter l_loggingRate = new RateLimiter(0.05);
+	public Gyro() {
+		loggerCategory = Category.GYRO;
+	}
+	public void resetGyro(){
+		g_gyro.setYaw(0);
+	}
+	public double getGyroPitch() {
+		double angle = g_gyro.getPitch();
+		return angle;
+		
+	}
+
+	public double getGyroYaw() {
+		double angle = g_gyro.getYaw();
+		return angle;
+	}
+
+	public double getGyroRoll() {
+		double angle = g_gyro.getRoll();
+		return angle;
+	}
+
+	@Override
+	public void run() {
+		 if (l_loggingRate.next()) {
+			 gyroArray[0] = getGyroYaw();
+			 gyroArray[1] = getGyroPitch();
+			 gyroArray[2] = getGyroRoll();
+			 g_gyro.getYawPitchRoll(gyroArray);
+		 	log("Yaw: " + gyroArray[0] + " || Pitch: " + gyroArray[1] + " || Roll: " + gyroArray[2]);
+		 }
+	}
+
+	/*private AHRS m_gyro;
 	private RateLimiter m_loggingRate = new RateLimiter(0.05);
 
 	public Gyro() {
@@ -51,5 +88,6 @@ public class Gyro extends Mechanism {
 		 if (m_loggingRate.next()) {
 		 	log("Gyro angle: " + getGyroYaw() + "Is connected: " + m_gyro.isConnected());
 		 }
-	}
+	}*/
+	
 }
